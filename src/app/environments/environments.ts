60b940679ebb9e7866bc775ee7a3063405ef3668
temic137 <68796483+temic137@users.noreply.github.com>
2024-11-08 00:25:20 +0100

    Update environments.ts

--- src/app/environments/environments.ts
+++ src/app/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     githubApiUrl: 'https://api.github.com', // Development API URL
-    githubToken:''
+    githubToken: process.env.GITHUB_TOKEN
   };
